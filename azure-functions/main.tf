de7b4ab66cc35d474b5a46b8e1491a063a418268
86777 <132754270+86777@users.noreply.github.com>
2023-06-18 13:54:35 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -4,17 +4,21 @@ resource "azurerm_resource_group" {
 }
 
 resource "azurerm_storage_account" {
-
+  name = "azure-functions-test-rg"
+  location = azurerm_resource_group.location
 }
 
 resource "azurerm_app_service_plan" {
-
+  name = "azure-functions-test-rg"
+  location = "azurerm_resource_group.location
 }
 
 resource "azurerm_linux_function_app" {
-
+  name = "azure-functions-test-rg"
+  location = azurerm_resource_group.location
 }
 
 resource "azurerm_function_app_function" {
-
+  name = "azure-functions-test-rg"
+  location = azurerm_resource_group.location
 }
