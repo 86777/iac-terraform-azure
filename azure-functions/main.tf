4f4a77f63a2900b0aac8d56867151324831183e4
86777 <132754270+86777@users.noreply.github.com>
2023-06-18 12:31:43 +0200

    main.tf

---
+++ azure-functions/main.tf
@@ -0,0 +1,8 @@
+resource "resource-group" {
+  azurerm_resource_group = 
+  azurerm_storage_account =
+  azurerm_app_service_plan =
+  azurerm_linux_function_app =
+  azurerm_function_app_function =
+
+}
