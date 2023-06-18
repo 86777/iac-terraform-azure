0e4b2800f3702c7e0f581540851e479e9e07a108
86777 <132754270+86777@users.noreply.github.com>
2023-06-18 14:07:48 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,24 +1,30 @@
 resource "azurerm_resource_group" "example" {
   name = "resource_group"
   location = "East Europe"
+  id = "001"
 }
 
 resource "azurerm_storage_account" "example" {
   name = "azurerm_storage_account"
+  resource_group_name = azurerm_resource_group.example.name
   location = azurerm_resource_group.example.location
 }
 
 resource "azurerm_app_service_plan" "example" {
   name = "azurerm_app_service_plan"
+  resource_group_name = azurerm_resource_group.example.name
   location = "azurerm_resource_group.example.location
+  app_service_plan_id = azurerm_app_service_plan.example.id
 }
 
 resource "azurerm_linux_function_app" "example" {
   name = "azurerm_linux_function_app"
+  resource_group_name = azurerm_resource_group.example.name
   location = azurerm_resource_group.example.location
 }
 
 resource "azurerm_function_app_function" "example" {
   name = "azurerm_function_app_function"
+  resource_group_name = azurerm_resource_group.example.name
   location = azurerm_resource_group.example.location
 }
