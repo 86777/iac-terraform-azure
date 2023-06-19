31e85fdf5d47f0faea1bc674a588b00dfcda3607
86777 <132754270+86777@users.noreply.github.com>
2023-06-19 17:10:28 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -1,6 +1,23 @@
-resource "resource-group" {
-  azurerm_resource_group =
-  azurerm_storage_account =
-  azurerm_storage_container =
-  azurerm_storage_blob =
+resource "azurerm_resource_group" "example" {
+  name = resource_group_name
+  location = "East Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name = storage_account_name
+  resource_group_name = azurerm_resource_group.example
+  location = azurerm_resource_group.example.location
+}
+
+resource "azurerm_storage_container" "example" {
+  name = storage_container_name
+  resource_group_name = azurerm_resource_group.example
+  storage_account_name = azurerm_storage_account.example
+}
+
+resource "azurerm_storage_blob" "example" {
+  name = storage_blob_name
+  resource_group_name = azurerm_resource_group.example
+  storage_account_name = azurerm_storage_account.example
+  storage_container_name = azurerm_storage_container.example
 }
