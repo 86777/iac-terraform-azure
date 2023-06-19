4352ec2187756bac22d8a13cb0ffb30ecf1f1a01
86777 <132754270+86777@users.noreply.github.com>
2023-06-19 17:01:37 +0200

    Update variables.tf

--- azure-functions/variables.tf
+++ azure-functions/variables.tf
@@ -1,1 +1,23 @@
+variable "resource_group_name" {
+  type = string
+}
 
+variable "storage_account_name" {
+  type = string
+}
+
+variable "app_service_plan_name" {
+  type = string
+}
+
+variable "function_app_name" {
+  type = string
+}
+
+variable "linux_function_app_name" {
+  type = string
+}
+
+variable "function_name" {
+  type = string
+}
